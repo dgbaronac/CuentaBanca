187c355f3f6cb0cb912a9413d9916b97cc7c63b6
Usuario5 <Usuario5@FI4531119-05.unal.loc>
2016-02-23 15:22:24 -0500

    Primera version del programa

---
+++ src/cuenta/Cuenta.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuenta;
+
+/**
+ *
+ * @author Usuario5
+ */
+public class Cuenta {
+
+    
+    
+    public static void main(String[] args) {
+        int p,q;
+        
+        Funcion m = new Funcion(1012,1234,"Margarita Sanchez",0);
+        
+        m.Consignar(100000);
+        
+    }
+    
+}
